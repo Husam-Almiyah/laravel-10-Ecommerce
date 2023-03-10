ca9dc560a5fa69f0fda810cfe8158beb4acea1d9
husamsmartminds <hossamalmayah19@gmail.com>
2023-03-10 19:46:30 +0300

    Adding items to the cart

--- app/Http/Livewire/ProductSelector.php
+++ app/Http/Livewire/ProductSelector.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use App\Cart\Contracts\CartInterface;
 use App\Models\Variation;
 use Livewire\Component;
 
@@ -35,9 +36,12 @@ class ProductSelector extends Component
 
     }
 
-    public function addToCart()
+    public function addToCart(CartInterface $cart)
     {
-        dd($this->skuVariant);
+
+        $cart->add($this->skuVariant, 1);
+        $this->emit('cart.updated');
+
     }
 
     public function render()
