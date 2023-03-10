3cb53a6712855c20af2faa1bb213305d4079ad37
husamsmartminds <hossamalmayah19@gmail.com>
2023-03-10 17:57:03 +0300

    Showing the cart in the navigation

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -19,7 +19,14 @@
             </div>
 
             <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ml-6">
+            <div class="hidden sm:flex sm:items-center sm:ml-6 space-x-5">
+                <a href="/cart" class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700
+                hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition
+                duration-150 ease-in-out">
+
+                    cart ({{ $this->cart->contentsCount() }})
+                    
+                </a>
                 @auth
                 {{-- @if (auth()->check()) --}}
                     
