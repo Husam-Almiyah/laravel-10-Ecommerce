672e02bbe15bff7e5213ee69181292c13654945b
husamsmartminds <hossamalmayah19@gmail.com>
2023-03-13 11:16:22 +0300

    Showing the category products page

---
+++ resources/views/categories/show.blade.php
@@ -0,0 +1,19 @@
+<x-app-layout>
+    <x-slot name="header">
+        <div class="space-x-1">
+            @foreach ($category->ancestors->reverse() as $ancestors)
+                <a href="/categories/{{ $ancestors->slug }}" class="text-indigo-500">
+                    {{ $ancestors->title }}
+                </a>
+
+                <span class="font-bold text-gray-300 last:hidden">/</span>
+            @endforeach
+        </div>
+
+        <h2 class="mt-1 font-semibold text-xl text-gray-800 leading-tight">
+            {{ $category->title }}
+        </h2>
+    </x-slot>
+
+    Product browser
+</x-app-layout>
