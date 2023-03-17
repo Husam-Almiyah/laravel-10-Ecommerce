92f5e2cb5d59a85b8cc22788b698cd398f21e6ab
husamsmartminds <hossamalmayah19@gmail.com>
2023-03-17 09:58:39 +0300

    Adding global navigation search

--- app/Http/Livewire/Navigation.php
+++ app/Http/Livewire/Navigation.php
@@ -3,10 +3,18 @@
 namespace App\Http\Livewire;
 
 use App\Cart\Contracts\CartInterface;
+use App\Models\Product;
 use Livewire\Component;
 
 class Navigation extends Component
 {
+    public $searchQuery = '';
+
+    public function clearSearch()
+    {
+        $this->searchQuery = '';
+    }
+
     protected $listeners = [
         'cart.updated' => '$refresh'
     ];
@@ -18,6 +26,10 @@ class Navigation extends Component
 
     public function render()
     {
-        return view('livewire.navigation');
+        $products = Product::search($this->searchQuery)->get();
+
+        return view('livewire.navigation', [
+            'products' => $products,
+        ]);
     }
 }
