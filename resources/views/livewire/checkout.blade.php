8c371a8669b9de4ff174ac4c5c6a6564924812f1
husamsmartminds <hossamalmayah19@gmail.com>
2023-03-19 16:24:59 +0300

    Calculating the cart totals

--- resources/views/livewire/checkout.blade.php
+++ resources/views/livewire/checkout.blade.php
@@ -116,7 +116,7 @@
                     <div class="space-y-1">
                         <div class="space-y-1 flex items-center justify-between">
                             <div class="font-semibold">Subtotal</div>
-                            <h1 class="font-semibold">$0</h1>
+                            <h1 class="font-semibold">{{ $cart->formattedSubtotal() }}</h1>
                         </div>
 
                         <div class="space-y-1 flex items-center justify-between">
@@ -126,7 +126,7 @@
 
                         <div class="space-y-1 flex items-center justify-between">
                             <div class="font-semibold">Total</div>
-                            <h1 class="font-semibold">$0</h1>
+                            <h1 class="font-semibold">{{ money($this->total) }}</h1>
                         </div>
                     </div>
 
