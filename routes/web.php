aaf2eb0b6a5c0eb6d91c4c4343114476e4a0a2f7
husamsmartminds <hossamalmayah19@gmail.com>
2023-03-24 10:57:33 +0300

    Showing the order confirmation page

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\CartIndexController;
 use App\Http\Controllers\CategoryShowController;
 use App\Http\Controllers\CheckoutIndexController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\OrderConfirmationIndexController;
 use App\Http\Controllers\ProductShowController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -28,6 +29,13 @@ Route::get('/checkout', CheckoutIndexController::class)->name('checkout');
 Route::get('/categories/{category:slug}', CategoryShowController::class);
 
 Route::get('/products/{product:slug}', ProductShowController::class);
+
+Route::get('/orders/{order:uuid}/confirmation', OrderConfirmationIndexController::class)
+    ->name('orders.confirmation');
+
+Route::get('/orders', function () {
+
+})->name('orders');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
