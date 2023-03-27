84e240451356006a2ed1ff6e5dfac02cf6c806fc
husamsmartminds <hossamalmayah19@gmail.com>
2023-03-27 10:45:40 +0300

    Returning the order status

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -24,12 +24,30 @@ class Order extends Model
         'shipped_at'
     ];
 
+    protected $casts = [
+        'placed_at' => 'datetime',
+        'packaged_at' => 'datetime',
+        'shipped_at' => 'datetime'
+    ];
+
+    public $statuses = [
+        'placed_at',
+        'packaged_at',
+        'shipped_at'
+    ];
+
     public static function booted()
     {
         static::creating(function (Order $order) {
-            $order->placed_at = $order->packaged_at = $order->shipped_at = now();
+            $order->placed_at = now();
             $order->uuid = (string) Str::uuid();
         });
+    }
+
+    public function status()
+    {
+        return collect($this->statuses)
+            ->last(fn ($status) => filled($this->{$status}));
     }
 
     public function formattedSubtotal()
